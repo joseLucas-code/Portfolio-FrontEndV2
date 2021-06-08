0cdf67ae63f6ac0f565db58b5438aef7b92e06a5
joseLucas-code <you@example.com>
2021-06-08 00:06:38 -0300

    Final

---
+++ js/index.js
@@ -0,0 +1,59 @@
+// toggle menu
+
+const toggleMenuBtn = document.querySelector('.toggle-menu')
+
+toggleMenuBtn.addEventListener('click', ()=>{
+    const navGrid = document.querySelector('.nav-grid')
+
+    navGrid.classList.toggle('open-menu')
+})
+
+// toggle theme
+
+const toggleThemeBtn = document.querySelector('.toogle-theme')
+
+toggleThemeBtn.addEventListener('click', ()=>{
+    localStorage.getItem('theme') === 'dark' ? localStorage.setItem('theme','light') : localStorage.setItem('theme','dark')   
+    document.body.classList.toggle('dark-theme')
+
+})
+
+function setTheme(){
+    if(localStorage.getItem('theme') === 'dark'){
+        document.body.classList.add('dark-theme')
+    }    
+}
+
+setTheme()
+
+// active each knowledge tabs
+
+const skillTab = document.querySelectorAll('article.front-skills')
+
+skillTab.forEach(tab => tab.addEventListener("click", ()=>{
+    
+    if(tab.classList.contains('active-tab')){
+        tab.classList.remove('active-tab')
+        
+    }else{
+        skillTab.forEach(single =>{
+            single.classList.remove('active-tab')
+        })
+        tab.classList.add('active-tab')
+    }
+
+}))
+
+// projects slider
+
+const projectSliderContainer = document.querySelector('.projects-slider')
+
+new Glider(projectSliderContainer,{
+    slidesToShow: 1,
+    draggable: false,
+    arrows:{
+        prev: '.glider-prev',
+        next: '.glider-next'
+    },
+    dots: '.dots'
+});
